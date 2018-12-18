9309ec7a05cf94b1d491a1361e8ed0eb8586fbfe
Andal01 <45972120+Andal01@users.noreply.github.com>
2018-12-18 09:10:13 -0600

    Initial Commit

---
+++ Test Map/Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour {
+
+	public NavMeshAgent nav;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Keep track of time passed
+		float t = Time.time - startTime;
+		//Alter speed based on time elapsed(in sec)
+		if (t < 60f) {
+			nav.speed = 1f;
+		} 
+		if (t >= 60f) {
+			nav.speed = 2f;
+		}
+		if (t >= 120f) {
+			nav.speed = 3f;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		GameObject.Find("TimerText").SendMessage("lossCondition");
+	}
+}
---
+++ Test Map/Assets/Scripts/TimePassed.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimePassed : MonoBehaviour {
+
+	public Text timeText;
+	private float startTime;
+	private bool lose = false;
+
+	void Start (){
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (lose)
+			return;
+		float t = Time.time - startTime;
+		string minutes = ((int)t / 60).ToString ();
+		string seconds = (t % 60).ToString ("f0");
+		timeText.text = minutes + ":" + seconds;
+	}
+
+	public void lossCondition()
+	{
+		lose = true;
+	}
+}
